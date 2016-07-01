ec371c32a12920dd4b85a6438f537fa7a9e431ff
Alex <kavourias.a@husky.neu.edu>
2016-07-01 19:26:17 -0400

    update height of google map

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -1,17 +1,5 @@
 function upload(file, signed_request, url, done) {
-      /*$.ajax(signed_request, {
-          type: "PUT",
-          url: signed_request,
-          dataType: file.type,
-          success: function(response) {
-            console.log(response);
-          },
-          error: function (xhr, status) {
-            console.log(status);
-          }
-      });*/
       var xhr = new XMLHttpRequest();
-      console.log('here: ' + signed_request);
       xhr.open("PUT", signed_request);
 
       xhr.setRequestHeader('x-amz-acl', 'public-read');
@@ -22,31 +10,37 @@ function upload(file, signed_request, url, done) {
         }
       };
       xhr.send(file);
+}
+
+function sign_request(file, done) {
+  var xhr = new XMLHttpRequest()
+  xhr.open("GET", "file_name=" + file.name + "&file_type=" + file.type)
+
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
+      var response = xhr.responseText;
+      done(response);
     }
+  }
+  xhr.send();
+}
 
-    function sign_request(file, done) {
-      var xhr = new XMLHttpRequest()
-      xhr.open("GET", "http://localhost:8000/photos/sign?file_name=" + file.name + "&file_type=" + file.type)
+document.getElementById("image").onchange = function() {
+  var files = document.getElementById("image").files;
+   
+  if (!files) return;
 
-      xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-          console.log(xhr.responseText);
-          var response = xhr.responseText;
-          done(response);
-        }
-      }
-      xhr.send();
-    }
-
-    document.getElementById("image").onchange = function() {
-      var file = document.getElementById("image").files[0];
-      if (!file) return;
-
-      sign_request(file, function(response) {
+  files.forEach(function(file) {
+    sign_request(file, function(response) {
         response = JSON.parse(response);
         console.log('Rsp:' + response);
         upload(file, response['signed_request'], response['url'], function() {
           document.getElementById("preview").src = response['url'];
+          $.post('/photos',
         });
-      })
-    }
+     });
+  }
+}
+
+
